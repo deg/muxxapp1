7c3c58e648f86cb03f7ac11903fa33817d18cf5a
David Goldfarb <deg@degel.com>
2014-01-29 15:28:00 +0200

    0.1.4 - More dependency madness.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject degel/mummy "0.1.3"
+(defproject degel/mummy "0.1.4"
   :description "A simple muxx project which I will use mostly to test new ideas"
   :url "https://github.com/deg/mummy"
   :license {:name "Eclipse Public License"
@@ -20,16 +20,16 @@
                  [org.clojure/clojurescript "0.0-2069"]
 
                  ;; Degel's Clojure utility library
-                 [degel/degel-clojure-utils "0.1.20" :exclusions [joda-time]]
+                 [degel/degel-clojure-utils "0.1.21"]
 
                  ;; Degel's website multiplexer
-                 [degel/muxx "0.1.6"]
+                 [degel/muxx "0.1.7"]
 
                  ;; Needed, I think, in order to build a runnable uberjar with compojure
                  [ring/ring-jetty-adapter "1.2.1"]
 
                  ;; Routing library for Ring web application library
-                 [compojure "1.1.5" :exclusions [ring/ring-core org.clojure/tools.macro]]
+                 [compojure "1.1.6" :exclusions [org.clojure/tools.macro]]
 
                  ;; DOM manipulation library for ClojureScript
                  [domina "1.0.2"]
@@ -46,10 +46,7 @@
                  [shoreleave/shoreleave-remote "0.3.0"]
 
                  ;; Clojure/ClojureScript validation
-                 [com.cemerick/valip "0.3.2"]
-
-                 ;; Testing from ClojureScript
-                 [com.cemerick/clojurescript.test "0.2.1"]]
+                 [com.cemerick/valip "0.3.2"]]
 
   :hooks [leiningen.cljsbuild]
 
@@ -58,9 +55,10 @@
             [com.cemerick/austin "0.1.3"]
 
             ;; Testing from ClojureScript
-            [com.cemerick/clojurescript.test "0.2.1"]]
+            [com.cemerick/clojurescript.test "0.2.2"]]
 
   :min-lein-version "2.0.0"
+  :pedantic? :abort
 
   :main degel.mummy.server ;; For standalone deployment
 
